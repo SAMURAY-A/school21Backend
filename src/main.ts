bb3070a968f41ec3352d216aa8c9983dc6a1d1a6
SAMURAY <abdullohaziz559@gmail.com>
2025-09-25 01:06:15 +0500

    finished

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3010);
+
+  // CORS yoqish
+  app.enableCors({
+    origin: '*', // yoki ['http://localhost:3000', 'https://seningsayting.uz']
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(3010);
 }
 bootstrap();
